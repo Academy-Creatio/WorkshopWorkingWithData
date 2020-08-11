f4f28dcdf49b238cc108e07052df0d8f81a14419
Kyryl Krylov, CPA <kirill.krylov@gmail.com>
2020-08-11 09:52:14 +0300

    Add project files.

---
+++ Files/DataOperations/ReadingData.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Data;
+using Terrasoft.Common;
+using Terrasoft.Core;
+using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
+
+namespace WorkshopWorkingWithData.Files.DataOperations
+{
+    internal sealed class ReadingData
+    {
+        UserConnection UserConnection;
+        public ReadingData(UserConnection userConnection)
+        {
+            UserConnection = userConnection;
+        }
+
+        #region Example: GetAllData
+        internal Tuple<DataTable, string> GetAllContactsEsq()
+        {
+            const string tableName = "Contact";
+            EntitySchemaQuery esqResult = new EntitySchemaQuery(UserConnection.EntitySchemaManager, tableName);
+            esqResult.PrimaryQueryColumn.IsVisible = true;
+            
+            esqResult.AddColumn("Name");
+            esqResult.AddColumn("Email");
+            esqResult.AddColumn("Phone");
+
+            Select select = esqResult.GetSelectQuery(UserConnection);
+            select.BuildParametersAsValue = true;
+
+            var entities = esqResult.GetEntityCollection(UserConnection);
+            return new Tuple<DataTable, string>(entities.ConvertToDataTable(), select.GetSqlText());
+        }
+        internal Tuple<DataTable, string> GetAllContactsCustomQuery()
+        {
+
+            CustomQuery custom = new CustomQuery(UserConnection, "Select Id, Name, Phone, Email from Contact");
+
+            DataTable dt;
+
+            using (DBExecutor dbExecutor = UserConnection.EnsureDBConnection(QueryKind.General))
+            {
+                using (IDataReader reader = custom.ExecuteReader(dbExecutor))
+                {
+                    dt = reader.ReadToDataTable("Contact");
+                }
+            }
+            custom.BuildParametersAsValue = true;
+            return new Tuple<DataTable, string>(dt, custom.GetSqlText());
+        }internal Tuple<DataTable, string> GetAllContactsSelect()
+        {
+            Select select = new Select(UserConnection)
+                .Column("Id").As("Id")
+                .Column("Contact", "Name").As("ContactName")
+                .Column("Email")
+                .Column("Phone")
+                .From("Contact");
+
+
+            DataTable dt;
+
+            using (DBExecutor dbExecutor = UserConnection.EnsureDBConnection(QueryKind.General))
+            {
+                using (IDataReader reader = select.ExecuteReader(dbExecutor))
+                {
+                    dt = reader.ReadToDataTable("Contact");
+                }
+            }
+            select.BuildParametersAsValue = true;
+            return new Tuple<DataTable, string>(dt, select.GetSqlText());
+        }
+
+        #endregion
+
+        #region Example: GetFilteredData
+        internal Tuple<DataTable, string> GetFilteredContactsSelect(string email)
+        {
+            Select select = new Select(UserConnection)
+                .Column("Id").As("Id")
+                .Column("Contact", "Name").As("ContactName")
+                .Column("Email")
+                .Column("Phone")
+                .From("Contact")
+                .Where("Email").IsEqual(Column.Parameter(email)) as Select;
+
+            DataTable dt;
+
+            using (DBExecutor dbExecutor = UserConnection.EnsureDBConnection(QueryKind.General))
+            {
+                using (IDataReader reader = select.ExecuteReader(dbExecutor))
+                {
+                    dt = reader.ReadToDataTable("Contact");
+                }
+            }
+            select.BuildParametersAsValue = true;
+            return new Tuple<DataTable, string>(dt, select.GetSqlText());
+        }
+
+        internal Tuple<DataTable, string> GetFilteredContactsEsq(string email)
+        {
+            const string tableName = "Contact";
+            EntitySchemaQuery esqResult = new EntitySchemaQuery(UserConnection.EntitySchemaManager, tableName);
+            esqResult.PrimaryQueryColumn.IsVisible = true;
+
+            esqResult.AddColumn("Name");
+            esqResult.AddColumn("Email");
+            esqResult.AddColumn("Phone");
+
+            IEntitySchemaQueryFilterItem filterByEmail = esqResult.CreateFilterWithParameters(FilterComparisonType.Equal, "Email", email);
+            esqResult.Filters.Add(filterByEmail);
+
+            Select select = esqResult.GetSelectQuery(UserConnection);
+            select.BuildParametersAsValue = true;
+
+            var entities = esqResult.GetEntityCollection(UserConnection);
+            return new Tuple<DataTable, string>(entities.ConvertToDataTable(), select.GetSqlText());
+        }
+        internal Tuple<DataTable, string> GetFilteredContactsCustomQuery(string email)
+        {
+        CustomQuery custom = new CustomQuery(UserConnection, $@"
+            Select Id, Name, Phone, Email 
+            from Contact
+            where Email ='{email}'");
+
+            DataTable dt;
+
+            using (DBExecutor dbExecutor = UserConnection.EnsureDBConnection(QueryKind.General))
+            {
+                using (IDataReader reader = custom.ExecuteReader(dbExecutor))
+                {
+                    dt = reader.ReadToDataTable("Contact");
+                }
+            }
+            custom.BuildParametersAsValue = true;
+            return new Tuple<DataTable, string>(dt, custom.GetSqlText());
+        }
+        #endregion
+    }
+}
---
+++ Files/WebServices/WorkshopWebService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using Terrasoft.Core;
+using Terrasoft.Web.Common;
+using WorkshopWorkingWithData.Files.DataOperations;
+
+namespace WorkshopWorkingWithData
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class WorkshopWebService : BaseService
+    {
+        private ReadingData ReadingData { get; set; }
+        private Stopwatch Timer { get; set;}
+        public WorkshopWebService()
+        {
+            ReadingData = new ReadingData(UserConnection ?? SystemUserConnection);
+            Timer = new Stopwatch();
+        }
+
+        #region Properties
+        private SystemUserConnection _systemUserConnection;
+        private SystemUserConnection SystemUserConnection
+        {
+            get
+            {
+                return _systemUserConnection ??= (SystemUserConnection)AppConnection.SystemUserConnection;
+            }
+        }
+        #endregion
+
+        #region Methods : REST
+        [OperationContract]
+        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Xml)]
+        public Stream GetAllContacts(QuryType qt)
+        {
+            //http://localhost:6090/0/rest/WorkshopWebService/GetAllContacts?qt=0
+            Timer.Start();
+            Tuple<DataTable, string> result;
+            switch (qt)
+            {
+                case QuryType.SELECT:
+                    result = ReadingData.GetAllContactsSelect();
+                    break;
+                case QuryType.ESQ:
+                    result = ReadingData.GetAllContactsEsq();
+                    break;
+                case QuryType.CustomQuery:
+                    result = ReadingData.GetAllContactsCustomQuery();
+                    break;
+                default:
+                    result = ReadingData.GetAllContactsSelect();
+                    break;
+            }
+            Timer.Stop();
+
+            string htmlPage = result.Item1.GetHtmlPage(Timer.ElapsedTicks, result.Item2, qt.ToString());
+            return GetMemoryStream(htmlPage);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Xml)]
+        public Stream GetFilteredContacts(QuryType qt, string email)
+        {
+            //http://localhost:6090/0/rest/WorkshopWebService/GetFilteredContacts?qt=0&email=a.baker@ac.com	
+            Timer.Start();
+            Tuple<DataTable, string> result;
+            switch (qt)
+            {
+                case QuryType.SELECT:
+                   result = ReadingData.GetFilteredContactsSelect(email);
+                    break;
+                case QuryType.ESQ:
+                    result = ReadingData.GetFilteredContactsEsq(email);
+                    break;
+                case QuryType.CustomQuery:
+                    result = ReadingData.GetFilteredContactsCustomQuery(email);
+                    break;
+                default:
+                    result = ReadingData.GetFilteredContactsSelect(email);
+                    break;
+            }
+            Timer.Stop();
+
+            string htmlPage = result.Item1.GetHtmlPage(Timer.ElapsedTicks, result.Item2, qt.ToString());
+            return GetMemoryStream(htmlPage);
+        }
+        #endregion
+
+        #region Methods : Private
+        private MemoryStream GetMemoryStream(string htmlPage)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(htmlPage);
+            string contentType = "text/html; charset=utf-8";
+            WebOperationContext.Current.OutgoingResponse.ContentType = contentType;
+            WebOperationContext.Current.OutgoingResponse.ContentLength = data.Length;
+            return new MemoryStream(data);
+        }
+        #endregion
+    }
+}
